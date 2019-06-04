d53bf56dbe56b24922ccfc4525e3b1d2959f74e2
Ionut Barau <ionutbarau@efficientix.go.ro>
2019-06-04 22:48:11 +0300

    Added some notes and classes for Builder

---
+++ src/com/learn/designpatterns/creational/builder/v1/CarBuilder.java
@@ -0,0 +1,10 @@
+package com.learn.designpatterns.creational.builder.v1;
+
+/**
+ * User: Ionut Barau (ionutbarau)
+ * Project: design-patterns
+ * Date: 2019-06-04.
+ * Time: 22:27
+ */
+public abstract class CarBuilder {
+}
---
+++ src/com/learn/designpatterns/creational/builder/v1/MercedesBuilder.java
@@ -0,0 +1,10 @@
+package com.learn.designpatterns.creational.builder.v1;
+
+/**
+ * User: Ionut Barau (ionutbarau)
+ * Project: design-patterns
+ * Date: 2019-06-04.
+ * Time: 22:33
+ */
+public class MercedesBuilder extends CarBuilder{
+}
